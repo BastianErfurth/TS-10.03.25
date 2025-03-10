952322b0c565c8cb46246327e05f27442a6acefc
Bastian Erfurth <bastian.erfurth01@gmail.com>
2025-03-10 11:17:28 +0100

    Aufgabe 1 gelöst

--- main.dart
+++ main.dart
@@ -1,6 +1,14 @@
+import 'dart:ffi';
+
 void main() {
-  for (int i = 0; i < 10; i++) {
-    int kleineZahl = (i);
-    print(kleineZahl);
+  for (int i = 0; i <= 100; i++) {
+    switch (i) {
+      case < 10:
+        print("kleine Zahl $i");
+      case < 60:
+        print("Mittlere Zahl $i");
+      default:
+        print("Große Zahl $i");
+    }
   }
 }
